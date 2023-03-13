2be897c7ae2e1f6b8a171f49307336df6bfdcb86
r3boot <rajsharma.9331@gmail.com>
2023-03-13 13:19:24 +0530

    project 3

---
+++ src/projects/Project_3.java
@@ -0,0 +1,19 @@
+package projects;
+
+public class Project_3 {
+	public static void main(String[] args) {
+		
+		int i = 10;
+
+		int j= 7; 
+
+		//double db1 = i/j; //1.0
+
+		double db1 = (double)i/j; //1.42
+//
+//	double db1 = i/(double)j; //1.42
+		
+		System.out.println(db1);
+		
+	}
+}
