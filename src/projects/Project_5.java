446d25354fa1b449f07b5f2ccea373548f147ede
r3boot <rajsharma.9331@gmail.com>
2023-03-13 13:27:57 +0530

    project_5

---
+++ src/projects/Project_5.java
@@ -0,0 +1,18 @@
+package projects;
+
+public class Project_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+
+		int j = ++i; 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+	}
+
+}
