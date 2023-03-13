e6085916a4bb1a455ca884761e693fa13b932ccf
r3boot <rajsharma.9331@gmail.com>
2023-03-13 13:37:11 +0530

    ASSIGNMENT 7

---
+++ src/projects/project_7.java
@@ -0,0 +1,20 @@
+package projects;
+
+public class project_7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+
+		int j = i++ + i++ + --i + ++i + ++i; 
+		
+		//      10   +  11  +  11 +  12  +  13 
+
+		System.out.println(i); 
+
+		System.out.println(j);
+
+	}
+
+}
