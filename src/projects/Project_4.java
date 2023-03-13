bf3cf0939dacc3fe5e575cf8044bd18546edf837
r3boot <rajsharma.9331@gmail.com>
2023-03-13 13:22:40 +0530

    Project 4

---
+++ src/projects/Project_4.java
@@ -0,0 +1,15 @@
+package projects;
+
+public class Project_4 {
+	public static void main(String[] args) {
+		
+		int i = 10; 
+
+		int j = i++; 
+
+		System.out.println("Value of i is "+i); 
+
+		System.out.println("Value of j is "+j);
+	}
+
+}
