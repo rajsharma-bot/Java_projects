e49b17479c9b1a55f3c005c9698b364d3c2718a2
r3boot <rajsharma.9331@gmail.com>
2023-03-13 14:39:19 +0530

    Assignment

---
+++ src/projects/Assignment_1.java
@@ -0,0 +1,14 @@
+package projects;
+
+public class Assignment_1 {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		/**
+		 * Output will be
+		 * 
+		 * Hello World
+		 */
+	}
+
+}
