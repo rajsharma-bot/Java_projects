e131d945b907b956b73341e6033e0b8fdab4848a
r3boot <rajsharma.9331@gmail.com>
2023-03-13 12:50:18 +0530

    Project_1

---
+++ src/projects/project_1.java
@@ -0,0 +1,25 @@
+package projects;
+
+/**
+ * @author rajsh
+ *
+ */
+public class project_1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		int i = 020;
+		int j = 20;
+		int k = 0X20;
+		
+		System.out.println("Value of i is " + i);
+		System.out.println("Value of j is " + j);
+		System.out.println("Value of k is " + k);
+	}
+
+}
