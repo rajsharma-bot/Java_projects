c9a1a7f112a175ec0868b6a36f8c85bf0a62410b
r3boot <rajsharma.9331@gmail.com>
2023-03-13 13:12:48 +0530

    Project 2

---
+++ src/projects/Project_2.java
@@ -0,0 +1,25 @@
+package projects;
+
+public class Project_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10; 
+
+		int j = 7; 
+
+		System.out.println("Sum "+(i+j)); 
+
+		System.out.println("Subtract "+ (i-j)); 
+
+		System.out.println("Multiplication "+(i*j)); 
+
+		System.out.println("Division " +(i/j)); 
+
+		System.out.println("Reminder " +(i%j));
+
+
+	}
+
+}
