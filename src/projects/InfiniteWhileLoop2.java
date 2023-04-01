4b13db5153b5daaa544f6b0b2eb461a279d9911c
r3boot <rajsharma.9331@gmail.com>
2023-04-01 18:12:14 +0530

    While loop

---
+++ src/projects/InfiniteWhileLoop2.java
@@ -0,0 +1,21 @@
+package projects;
+
+public class InfiniteWhileLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 1;
+		while (i == 1) {
+			// while loop is executed if the condition is true else the while loop is
+			// terminated.
+			// in our case we have entered condition is i==1 its always true, so condition will be never
+			// false
+			// and loop will run Infinite times
+
+			System.out.println("Infinite While 2");
+		}
+
+	}
+
+}
