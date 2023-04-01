0b3e6e391ce7e74f385e19d677860e7bb0170131
r3boot <rajsharma.9331@gmail.com>
2023-04-01 18:12:28 +0530

    For loop

---
+++ src/projects/InfiniteForLoop2.java
@@ -0,0 +1,19 @@
+package projects;
+
+public class InfiniteForLoop2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for (;;) {
+			
+			//Since there is no instance variable 
+			// in for loop there is no condition 
+			// Every time loop will run but there is no instance variable, condition & incr/decr
+			
+			System.out.println("Infinite Loop 2");
+		}
+
+	}
+
+}
