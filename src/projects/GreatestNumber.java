5d982345ba98f19357c13003ef06cd7649d3356d
r3boot <rajsharma.9331@gmail.com>
2023-04-02 19:27:25 +0530

    GreatestNumber

---
+++ src/projects/GreatestNumber.java
@@ -0,0 +1,28 @@
+package projects;
+
+import java.util.Scanner;
+
+public class GreatestNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1st number");
+		int a= sc.nextInt();
+		System.out.println("Enter 2nd number");
+		int b=sc.nextInt();
+		System.out.println("Enter 3rd number");
+		int c=sc.nextInt();
+		
+		if(a>b && a>c) {
+			System.out.println(a+" is the largest number");
+		} else if (b>a && b>c) {
+			System.out.println(b+" is the largest number");
+		}else{
+			System.out.println(c+" is the largest number");
+		}
+
+	}
+
+}
