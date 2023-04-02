258a321db5a390031cfdf35626d375e3d6134e7e
r3boot <rajsharma.9331@gmail.com>
2023-04-02 20:18:00 +0530

    Get number of days in month

--- src/projects/GetDayInMonths.java
+++ src/projects/GetDayInMonths.java
@@ -6,48 +6,44 @@ public class GetDayInMonths {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Please enter month in number to know days in month");
-		int month=sc.nextInt();
+		int month = sc.nextInt();
 		System.out.println("Please Enter Year for month");
-		int year=sc.nextInt();
-		
-	
-		
-		if(month == 1) {
+		int year = sc.nextInt();
+
+		if (month == 1) {
 			System.out.println("January having 31 days");
-		} else if(month == 2) {
-			if(year%4 ==0) {
+		} else if (month == 2) {
+			if (year % 4 == 0) {
 				System.out.println("February having 29 days");
-			}else {
+			} else {
 				System.out.println("February having 28 days");
 			}
-			
-		}
-		else if(month == 3) {
+
+		} else if (month == 3) {
 			System.out.println("March having 31 days");
-		}else if(month == 4) {
+		} else if (month == 4) {
 			System.out.println("April having 31 days");
-		}else if(month == 5) {
+		} else if (month == 5) {
 			System.out.println("May having 31 days");
-		}else if(month == 6) {
+		} else if (month == 6) {
 			System.out.println("June having 31 days");
-		}else if(month == 7) {
+		} else if (month == 7) {
 			System.out.println("July having 31 days");
-		}else if(month == 8) {
+		} else if (month == 8) {
 			System.out.println("August having 31 days");
-		}else if(month == 9) {
+		} else if (month == 9) {
 			System.out.println("September having 31 days");
-		}else if(month == 10) {
+		} else if (month == 10) {
 			System.out.println("October having 31 days");
-		}else if(month == 11) {
+		} else if (month == 11) {
 			System.out.println("November having 31 days");
-		}else if(month == 12) {
+		} else if (month == 12) {
 			System.out.println("January having 31 days");
-		}else {
+		} else {
 			System.out.println("Please enter valid Month range");
 		}
-		
 
 	}
 
