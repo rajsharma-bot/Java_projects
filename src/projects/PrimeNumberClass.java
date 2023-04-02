95634ee51d7b4b30d7c8f7f6a2b33730b5a88bac
r3boot <rajsharma.9331@gmail.com>
2023-04-02 21:16:55 +0530

    Prime number

--- src/projects/PrimeNumberClass.java
+++ src/projects/PrimeNumberClass.java
@@ -1,9 +1,14 @@
 package projects;
 
+import java.util.Scanner;
+
 public class PrimeNumberClass {
 
 	public static void main(String[] args) {
-		int i = 23;
+		
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Plese Enter number to check whether its prime number or not");
+		int i = sc.nextInt();
 		boolean isNumberPrime = false;
 		for (int j = 2; j < i - 1; j++) {
 			if (i % j == 0) {
@@ -11,7 +16,7 @@ public class PrimeNumberClass {
 				break;
 			}
 		}
-		if (isNumberPrime) {
+		if (isNumberPrime== false) {
 			System.out.println(i + " is prime");
 		} else {
 			System.out.println(i + " is not prime");
